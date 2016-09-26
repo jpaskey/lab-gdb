a4a7f82c6d26cc5037de817de793bad01ccb99a8
Bo Mei <bomei@me.com>
2016-09-26 00:50:40 -0400

    Create ReadFile.c

---
+++ Code/ReadFile.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE *openFile(char *name) {
+    FILE *f = fopen(name, "r");
+    if(f == NULL) {
+        printf("Cannot open %s\n", name);
+        exit(1);
+    }
+    return f;
+}
+
+int copyFile(FILE *f, char *target[], int maxLineCnt) {
+    int i;
+    char temp[100];
+    for(i = 0; i < maxLineCnt; i++) {
+        if(fgets(temp, 100, f) == NULL) break;
+        strcpy(target[i], temp);
+    }
+    return i + 1;
+}
+
+int main() {
+    char *lines[20];
+    int lineCnt;
+    FILE *file = openFile("Sunset.txt");
+    lineCnt = copyFile(file, lines, 20);
+    fclose(file);
+    printf("Total of lines: %d\n", lineCnt);
+    return 0;
+}
